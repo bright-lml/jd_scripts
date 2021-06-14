57c54bc1edb26da8e67fd971019626a739b7f81d
bright-lml <49701562+bright-lml@users.noreply.github.com>
2021-06-14 16:14:33 +0800

    Add files via upload

--- jd_cash.js
+++ jd_cash.js
@@ -31,8 +31,10 @@ let helpAuthor = true;
 const randomCount = $.isNode() ? 5 : 5;
 let cash_exchange = false;//是否消耗2元红包兑换200京豆，默认否
 const inviteCodes = [
-  `eU9YL5XqGLxSmRSAkwxR@eU9YaO7jMvwh-W_VzyUX0Q@eU9YaurkY69zoj3UniVAgg@eU9YaOnjYK4j-GvWmXIWhA@eU9YMZ_gPpRurC-foglg@eU9Ya77gZK5z-TqHn3UWhQ@eU9Yaui2ZP4gpG-Gz3EThA@eU9YaeizbvQnpG_SznIS0w`,
-  `-4msulYas0O2JsRhE-2TA5XZmBQ@eU9Yar_mb_9z92_WmXNG0w@eU9YaO7jMvwh-W_VzyUX0Q@eU9YaurkY69zoj3UniVAgg@eU9YaOnjYK4j-GvWmXIWhA@eU9YaO23bvtyozuGyHsR1A`
+  `eU9Ya-q2YqglpD_RyiVH1Q@eU9YPY3rMap1rDGBtixy@IhMxa-W1ZvQh9266iw`,
+  `Ih4-buuyZ_Qu8Wm6iw@eU9YPY3rMap1rDGBtixy@IhMxa-W1ZvQh9266iw`,
+  `Ih4-buuyZ_Qu8Wm6iw@eU9Ya-q2YqglpD_RyiVH1Q@eU9YPY3rMap1rDGBtixy`,
+  `Ih4-buuyZ_Qu8Wm6iw@eU9Ya-q2YqglpD_RyiVH1Q@IhMxa-W1ZvQh9266iw`
 ]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
